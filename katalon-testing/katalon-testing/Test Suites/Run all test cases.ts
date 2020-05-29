a980a9e090824e082e88cff8125dabb923524a22
thi174hcmus <thi174hcmus@gmail.com>
2020-05-29 15:55:51 +0700

    Add test case: Login

---
+++ katalon-testing/katalon-testing/Test Suites/Run all test cases.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run all test cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8fd26ec6-ed09-4e5a-bcbb-3a63530359ed</testSuiteGuid>
+   <testCaseLink>
+      <guid>fa27b014-5f2b-453c-b690-7c9788a2cc2f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserLogin</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>78965324-8e1c-486d-9273-1c7f5b654c86</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/UserAccounts</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>78965324-8e1c-486d-9273-1c7f5b654c86</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>unameOrEmail</value>
+         <variableId>7805109e-614c-496a-a30d-a79ecc85fbc1</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>78965324-8e1c-486d-9273-1c7f5b654c86</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>08c7b652-5af2-4eaf-98bc-e295fce2172d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
