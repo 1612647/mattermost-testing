4a8ced6c4eb8731492ae8cb3b178a0cdd5dd47bc
thi174hcmus <thi174hcmus@gmail.com>
2020-06-05 17:00:39 +0700

    Add test case: Logout

---
+++ katalon-testing/katalon-testing/Test Suites/Data driven test cases/Run Rename Chanels.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run Rename Chanels</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ccc1fdd4-5760-487a-a98f-33c1d329e77e</testSuiteGuid>
+   <testCaseLink>
+      <guid>d862ca22-be43-4c8a-ba89-3970ae065adc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Rename Chanel</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>86be008e-fd6b-42f2-af8d-8414ad8db084</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Rename Chanels</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>86be008e-fd6b-42f2-af8d-8414ad8db084</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>name</value>
+         <variableId>6956e317-7dd4-4361-82b6-9d965107a4a0</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
