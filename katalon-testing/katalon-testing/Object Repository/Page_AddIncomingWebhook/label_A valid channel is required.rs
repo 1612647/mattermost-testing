175f468ac04b2b039553277b44c1b5e93dd7f50e
thi174hcmus <thi174hcmus@gmail.com>
2021-05-16 14:03:06 +0700

    Add test case: Add Incoming Webhook

---
+++ katalon-testing/katalon-testing/Object Repository/Page_AddIncomingWebhook/label_A valid channel is required.rs
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_A valid channel is required</name>
+   <tag></tag>
+   <elementGuidId>a2721616-8d53-4057-876e-ee6172cc299f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div/div[2]/div[2]/div[2]/form/div[5]/div/label</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>label</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>control-label</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>A valid channel is required</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;backstage&quot;]/div[@class=&quot;backstage-body&quot;]/div[@class=&quot;backstage-content&quot;]/div[@class=&quot;backstage-form&quot;]/form[@class=&quot;form-horizontal&quot;]/div[@class=&quot;backstage-form__footer&quot;]/div[@class=&quot;pull-left has-error&quot;]/label[@class=&quot;control-label&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div[2]/div[2]/div[2]/form/div[5]/div/label</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='If set, the incoming webhook can post only to the selected channel.'])[1]/following::label[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Lock to this channel'])[1]/following::label[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cancel'])[1]/preceding::label[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/div/label</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ katalon-testing/katalon-testing/Scripts/Data Driven Test Cases/Add Incoming Webhook/Script1591358546641.groovy
@@ -0,0 +1,60 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.callTestCase(findTestCase('Data Driven Test Cases/User Login'), [('unameOrEmail') : 'user-1', ('password') : 'SampleUs@r-1'], 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/button_user-1_style--none sidebar-header-dr_eabf2e'), 0)
+
+WebUI.click(findTestObject('Page_AddIncomingWebhook/button_user-1_style--none sidebar-header-dr_eabf2e'))
+
+WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/span_Integrations'), 0)
+
+WebUI.click(findTestObject('Page_AddIncomingWebhook/span_Integrations'))
+
+WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/a_Incoming WebhooksIncoming webhooks allow _f6560d'), 0)
+
+WebUI.click(findTestObject('Page_AddIncomingWebhook/a_Incoming WebhooksIncoming webhooks allow _f6560d'))
+
+WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/button_Add Incoming Webhook'), 0)
+
+WebUI.click(findTestObject('Page_AddIncomingWebhook/button_Add Incoming Webhook'))
+
+WebUI.setText(findTestObject('Page_AddIncomingWebhook/input_Title_displayName'), title)
+
+WebUI.setText(findTestObject('Page_AddIncomingWebhook/input_Description_description'), description)
+
+WebUI.selectOptionByValue(findTestObject('Page_AddIncomingWebhook/select_--- Select a channel ---Off-TopicPub_c22433'), 
+    '9cwgocg9zbroxrfcrtdkitpige', true)
+
+if (isLock == 'true') {
+	WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/input_Lock to this channel_channelLocked'), 0)
+	WebUI.click(findTestObject('Page_AddIncomingWebhook/input_Lock to this channel_channelLocked'))
+}
+WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/span_Save'), 0)
+
+WebUI.click(findTestObject('Page_AddIncomingWebhook/span_Save'))
+
+WebUI.verifyElementNotPresent(findTestObject('Page_AddIncomingWebhook/label_A valid channel is required'), 1)
+
+WebUI.scrollToElement(findTestObject('Page_AddIncomingWebhook/span_Done'), 0)
+
+WebUI.click(findTestObject('Page_AddIncomingWebhook/span_Done'))
+
+WebUI.closeBrowser()
+
