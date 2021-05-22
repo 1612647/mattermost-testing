b85e014673e3cd53a05a39e624777f9a0cb13a1d
thi174hcmus <thi174hcmus@gmail.com>
2021-05-22 18:03:06 +0700

    Add test case: Remove direct Messages

---
+++ katalon-testing/katalon-testing/Object Repository/Page_Remove Direct Messages/btn_close_newItem.rs
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_close_newItem</name>
+   <tag></tag>
+   <elementGuidId>1cf5ce5b-e3d6-4654-9095-ae8b68e5f1bb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@id='directChannelList']/li[@class='active']/div/a/span[@class='btn-close']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <type>Main</type>
+      <value>//ul[@id='directChannelList']/li[@class='active']/div/a/span[@class='btn-close']</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ katalon-testing/katalon-testing/Object Repository/Page_Remove Direct Messages/newItem.rs
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>newItem</name>
+   <tag></tag>
+   <elementGuidId>40d1c036-7649-479e-932d-b1ace5f530e7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;directChannelList&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <type>Main</type>
+      <value>//*[@id=&quot;directChannelList&quot;]/li[contains(@class, 'active')]/a</value>
+   </webElementXpaths>
+</WebElementEntity>
